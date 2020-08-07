6b9b074693bab39880ae8f26711563a3e582c68c
binh-dam-ibigroup <56846598+binh-dam-ibigroup@users.noreply.github.com>
2020-08-07 15:36:45 -0400

    refactor(TimeColumnWithDelay.story): Refactor story.

--- packages/itinerary-body/src/TimeColumnWithDelay.story.js
+++ packages/itinerary-body/src/TimeColumnWithDelay.story.js
@@ -1,16 +1,10 @@
 import { itineraryType } from "@opentripplanner/core-utils/lib/types";
 import ClassicLegIcon from "@opentripplanner/icons/lib/classic-leg-icon";
-import TriMetLegIcon from "@opentripplanner/icons/lib/trimet-leg-icon";
-import PropTypes from "prop-types";
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 import { withInfo } from "@storybook/addon-info";
-import { action } from "@storybook/addon-actions";
 
-import DefaultLineColumnContent from "./defaults/line-column-content";
-import DefaultPlaceName from "./defaults/place-name";
-import DefaultRouteDescription from "./defaults/route-description";
 import DefaultTransitLegSummary from "./defaults/transit-leg-summary";
 import { WrappedOtpRRTransitLegSubheader } from "./demos";
 import OtpRRStyledItineraryBody from "./otp-react-redux/itinerary-body";
@@ -28,89 +22,26 @@ const walkTransitWalkRealtimeDelayedItinerary = require("./__mocks__/itineraries
 const walkTransitWalkRealtimeEarlyItinerary = require("./__mocks__/itineraries/walk-transit-walk-fdot-realtime-early.json");
 const walkTransitWalkRealtimeNearOntimeItinerary = require("./__mocks__/itineraries/walk-transit-walk-fdot-realtime-near-ontime.json");
 
-const ItineraryBodyDefaultsWrapper = ({
-  itinerary,
-  LegIcon,
-  LineColumnContent,
-  PlaceName,
-  RouteDescription,
-  showAgencyInfo,
-  showLegIcon,
-  showMapButtonColumn,
-  showViewTripButton,
-  toRouteAbbreviation,
-  TransitLegSubheader,
-  TransitLegSummary
-}) => (
+const OtpRRItineraryBodyWrapper = ({ itinerary }) => (
   <OtpRRStyledItineraryBody
     config={config}
-    frameLeg={action("frameLeg")}
     itinerary={itinerary}
-    LegIcon={LegIcon}
-    LineColumnContent={LineColumnContent || DefaultLineColumnContent}
-    PlaceName={PlaceName || DefaultPlaceName}
-    RouteDescription={RouteDescription || DefaultRouteDescription}
+    LegIcon={ClassicLegIcon}
+    LineColumnContent={OtpRRLineColumnContent}
+    PlaceName={OtpRRPlaceName}
+    RouteDescription={OtpRRRouteDescription}
     routingType="ITINERARY"
-    setActiveLeg={action("setActiveLeg")}
-    setViewedTrip={action("setViewedTrip")}
-    showAgencyInfo={showAgencyInfo}
-    showElevationProfile
-    showLegIcon={showLegIcon}
-    showMapButtonColumn={showMapButtonColumn}
-    showViewTripButton={showViewTripButton}
+    showAgencyInfo
+    showLegIcon
+    showMapButtonColumn={false}
+    showViewTripButton
+    styledItinerary="otp-rr"
     TimeColumnContent={TimeColumnWithDelays}
-    toRouteAbbreviation={toRouteAbbreviation}
-    TransitLegSubheader={TransitLegSubheader}
-    TransitLegSummary={TransitLegSummary || DefaultTransitLegSummary}
+    toRouteAbbreviation={r => r.toString().substr(0, 2)}
+    TransitLegSubheader={WrappedOtpRRTransitLegSubheader}
+    TransitLegSummary={DefaultTransitLegSummary}
   />
 );
-
-ItineraryBodyDefaultsWrapper.propTypes = {
-  itinerary: itineraryType.isRequired,
-  LegIcon: PropTypes.elementType,
-  LineColumnContent: PropTypes.elementType,
-  PlaceName: PropTypes.elementType,
-  RouteDescription: PropTypes.elementType,
-  showAgencyInfo: PropTypes.bool,
-  showLegIcon: PropTypes.bool,
-  showMapButtonColumn: PropTypes.bool,
-  showViewTripButton: PropTypes.bool,
-  toRouteAbbreviation: PropTypes.func,
-  TransitLegSubheader: PropTypes.elementType,
-  TransitLegSummary: PropTypes.elementType
-};
-
-ItineraryBodyDefaultsWrapper.defaultProps = {
-  LegIcon: TriMetLegIcon,
-  LineColumnContent: undefined,
-  PlaceName: undefined,
-  RouteDescription: undefined,
-  showAgencyInfo: false,
-  showLegIcon: false,
-  showMapButtonColumn: true,
-  showViewTripButton: false,
-  toRouteAbbreviation: r => r.toString().substr(0, 2),
-  TransitLegSubheader: undefined,
-  TransitLegSummary: undefined
-};
-
-function OtpRRItineraryBodyWrapper({ itinerary }) {
-  return (
-    <ItineraryBodyDefaultsWrapper
-      itinerary={itinerary}
-      LegIcon={ClassicLegIcon}
-      LineColumnContent={OtpRRLineColumnContent}
-      PlaceName={OtpRRPlaceName}
-      RouteDescription={OtpRRRouteDescription}
-      showAgencyInfo
-      showLegIcon
-      showMapButtonColumn={false}
-      showViewTripButton
-      styledItinerary="otp-rr"
-      TransitLegSubheader={WrappedOtpRRTransitLegSubheader}
-    />
-  );
-}
 
 OtpRRItineraryBodyWrapper.propTypes = {
   itinerary: itineraryType.isRequired
